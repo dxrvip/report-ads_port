d4fce774c2a6a25a116d6cb0aad7d7260bbbd80a
dxren <dzjvip@163.com>
2023-09-20 11:28:00 +0800

    and site_id != 'null'

--- backend/app/api/report.py
+++ backend/app/api/report.py
@@ -53,8 +53,8 @@ async def create_report(
 
     post: Optional[Post | None] = await crud.create_post(session, href, slug, domain)
     # 1, 不是taboola进入，2，带site——id进入，3，没有任何tab信息
-    if is_taboola or site_id: # 1, 不是taboola进入，2，带site——id进入
-        if not is_taboola and site_id != 'null':
+    if is_taboola or (site_id != 'null'): # 1, 不是taboola进入，2，带site——id进入
+        if not is_taboola:
             taboola_in = {"site_id":site_id}
         else:
             taboola_in = taboola_in.dict()
